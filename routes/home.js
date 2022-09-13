d6f516a3c5192ba7c6b73475d91cca68e1308d08
Diksha Padiyar <95542633+dikshapadi@users.noreply.github.com>
2022-09-13 23:25:44 +0530

    Add files via upload

---
+++ routes/home.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+const Review = require("../models/reviewUs.js");
+
+
+router.get("/", (req,res) => {
+	res.render("home/main");
+});
+
+router.get("/home/reviewus", (req,res) => {
+	res.render("home/reviewus");
+});
+
+router.post("/home/reviewus", async (req,res) => {
+	try
+	{
+		const review = req.body.review;
+		const newReview = new Review(review);
+		await newReview.save();
+		req.flash("success", "Review sent successfully");
+		res.redirect("/home/reviewus");
+	}
+	catch(err)
+	{
+		console.log(err);
+		req.flash("error", "Some error occurred on the server.")
+		res.redirect("back");
+	}
+});
+
+module.exports = router;	
